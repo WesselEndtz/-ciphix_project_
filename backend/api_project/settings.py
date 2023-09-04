64378b1038af2adb2b9a13404e65fa06c236ecbf
Wessel E <wesselendtz1@gmail.com>
2023-09-04 15:35:56 +0200

    Ready for deployment azure

--- backend/api_project/settings.py
+++ backend/api_project/settings.py
@@ -17,9 +17,10 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+SECRET_KEY = '38293'
+DEBUG = False
+ALLOWED_HOSTS = ['zealous-bush-56a466555bdf407f91ebbbfb7401977b.azurewebsites.net', 'yourcustomdomain.com']
+#os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
