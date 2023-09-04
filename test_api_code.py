d7fb3859f37899e9810a25589ec21dce0b4ccc4b
Wessel E <wesselendtz1@gmail.com>
2023-09-04 09:37:24 +0200

    tested url on localhost and tested with test_api_code.py and both succesfull

---
+++ test_api_code.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+# Define the URL of your API endpoint
+url = 'http://127.0.0.1:8000/contact/'
+
+# Define the data payload as a dictionary
+data = {
+    'name': 'Bobby Stearman',
+    'message': 'test',
+    'email': 'bobby@didcoding.com'
+}
+
+# Set the headers for the request
+headers = {
+    'Content-type': 'application/json'
+}
+
+# Convert the data payload to JSON format
+json_data = json.dumps(data)
+
+# Make a POST request using requests
+response = requests.post(url, data=json_data, headers=headers)
+
+# Check the response status code and content
+if response.status_code == 200:
+    print('POST request successful!')
+    print(response.json())
+else:
+    print(f'POST request failed with status code: {response.status_code}')
+    print(response.text)
