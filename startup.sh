e302c649a1eb76e84d9557c864f10dc6e6e1e38e
Wessel E <wesselendtz1@gmail.com>
2023-09-05 17:46:10 +0200

    added startup.sh

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Navigate to the root directory of your Django project
+cd /ciphix_project_/backend
+python manage.py runserver
+
+"""
+# Activate the virtual environment (if you have one)
+# source /path/to/your/virtualenv/bin/activate
+
+# Install/update Python packages
+pip install -r requirements.txt
+
+# Apply Django database migrations
+python manage.py migrate
+
+# Collect static files (if needed)
+# python manage.py collectstatic --noinput
+
+# Start Gunicorn application server
+gunicorn your_project_name.wsgi:application -b 0.0.0.0:8000
+"""
