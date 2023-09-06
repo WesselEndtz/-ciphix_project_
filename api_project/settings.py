338e0893731d5f103f440755f11233fbde54d573
Wessel E <wesselendtz1@gmail.com>
2023-09-06 13:52:36 +0200

    fixed small bug in settings.py

--- api_project/settings.py
+++ api_project/settings.py
@@ -121,16 +121,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+"""
 STATICFILES_DIRS = (
     os.path.join(STATIC_ROOT, 'js'),
     os.path.join(STATIC_ROOT, 'css'),
     os.path.join(STATIC_ROOT, 'images'),
     os.path.join(STATIC_ROOT, 'fonts'),
 )
-
+"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
