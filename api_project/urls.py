5e4af02dc88447a72f8794af49f1b7b498eba0c6
Wessel E <wesselendtz1@gmail.com>
2023-09-07 11:05:40 +0200

    added static url to urls.py

--- api_project/urls.py
+++ api_project/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from django.contrib import admin
 from core import views as core_views
 from rest_framework import routers
-
+from django.conf.urls.static import static
+from api_project import settings
 
 
 router = routers.DefaultRouter()
@@ -12,4 +13,4 @@ urlpatterns = router.urls
 urlpatterns += [
     path('admin/', admin.site.urls),
     path('txt_sentiment/', core_views.SentimentAPIView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
